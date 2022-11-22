60b812a15b3912afe36679652d8fa625aea126bf
EverythingWindows <windowseverything61@gmail.com>
2022-11-22 14:45:19 +0700

    story line for chapter 2

--- ShiftOS-TheRevival/Functions/InGame/LoreManager/IntroStoryTell.vb
+++ ShiftOS-TheRevival/Functions/InGame/LoreManager/IntroStoryTell.vb
@@ -1,7 +1,7 @@
 ﻿Module IntroStoryTell
     Public Sub StoryLineIntro(Timestamp As Integer)
         Select Case Strings.ComputerInfo(3)
-            Case "0"
+            Case 0
                 Select Case Timestamp
                     Case 5
                         ResetLine("Connected to <null>")
@@ -36,6 +36,35 @@
                         Terminal_PrintPrompt()
                         Terminal_AssignPrompt()
                         TextRebind()
+                End Select
+            Case 1
+                Select Case Timestamp
+                    Case 5
+                        ResetLine("<null> Connected")
+                    Case 25
+                        NewLine("<null> : Hmmm, you've got your terminal explored enough, ay?")
+                    Case 75
+                        NewLine("<null> : I've seen your progress and let's just say, it's quite rough for me.")
+                    Case 145
+                        NewLine("<null> : Having to code each functions, programs, and applications while you are using it, haha.")
+                    Case 180
+                        NewLine("<null> : I think you've already push the boundary of command line interface for ShiftOS.")
+                    Case 220
+                        NewLine("<null> : Let me introduce you to my basic Window Manager for ShiftOS, DuWM.")
+                    Case 270
+                        NewLine("<null> : It is capable for handling 2 programs at the same time.")
+                    Case 320
+                        NewLine("<null> : And the window manager will evolve as you usually using it.")
+                    Case 380
+                        NewLine("<null> : Wonder why you cannot send a message to me while I'm communicating with you?")
+                    Case 420 'That's the number baby!
+                        NewLine("<null> : Because I only design this to do one-way communication, no reply whatsover.")
+                    Case 475
+                        NewLine("<null> : Anyways, you gotta have 200 codepoints to install DuWM")
+                    Case 550
+                        NewLine("<null> : Once you able to afford it and installing it, there's a guide to use the window manager")
+                    Case 600
+                        NewLine("<null> : Alright, I gotta go now and see you in the next progress.")
                 End Select
         End Select
     End Sub
